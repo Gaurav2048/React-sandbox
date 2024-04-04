6e3f2df73d5c8ae2a90a02922817a8ed566d76e0
crio-gunjan <gunjan@crio-users.in>
2024-04-04 23:36:18 +0530

    Add button and loader component

---
+++ client/src/Components/Button/index.tsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { styled } from "styled-components";
+import Loader from "../Loader";
+
+type ButtonProps = {
+    children: any;
+    loading?: boolean;
+    onClick?: () => void;
+    variant: "contained" | "outlined" | "text" | "icon-button"
+    size: "sm" | "md" | "lg"
+    type: "primary" | "secondary"
+    fullWidth?: boolean
+    disabled?: boolean
+}
+
+
+const sizes = {
+    sm: {
+        padding: "4px 12px",
+        fontSize: "12px",
+        borderRadius: "4px"
+    },
+    md: {
+        padding: "12px 16px",
+        fontSize: "12px",
+        borderRadius: "4px"
+    },
+    lg: {
+        padding: "16px 24px",
+        fontSize: "12px",
+        borderRadius: "6px"
+    }
+}
+
+const variants = {
+    primary: {
+        background: "#cbcbcb",
+        contrast: "#f00"
+    },
+    secondary: {
+        border: "#cbcbcb",
+        contrast: "#f00"
+    }
+}
+
+
+const ContainedButton = styled.button<any> `
+    padding: ${props => props.padding};
+    width: ${props => props?.fullWidth ? '100%' : null}
+    font-size: ${props => props.fontSize};
+    background: ${props => props.theme.colors[props.type] }
+    color: white;
+    border-radius: ${props => props.borderRadius}
+`
+
+const OutlinedButton = styled.button<any> `
+    padding: ${props => props.padding};
+    width: ${props => props?.fullWidth ? '100%' : null}
+    font-size: ${props => props.fontSize};
+    border: 2px solid ${props => props.theme.colors[props.type] }
+    color: white;
+    border-radius: ${props => props.borderRadius}
+`
+
+const Button: React.FC<ButtonProps> = ({
+    variant, type, size, onClick, loading, children
+}) => {
+    if (variant === "contained") {
+        return <ContainedButton {...sizes[size]} type={type} onClick={onClick}>
+                {loading ? <Loader size={size} variant="primary" /> : null}{children}
+            </ContainedButton>
+    }
+    
+    if (variant === "outlined") {
+        return <OutlinedButton {...sizes[size]} type={type} onClick={onClick}>
+                {loading ? <Loader size={size} variant="primary" /> : null}{children}
+            </OutlinedButton>
+    }
+
+    if (variant === "icon-button") {
+        return <ContainedButton {...sizes[size]} type={type} onClick={onClick}>
+                {loading ? <Loader size={size} variant="primary" /> : null}{children}
+            </ContainedButton>
+    }
+
+    return null
+}
+
+export default Button;
+
+
---
+++ client/src/Components/Loader/Loader.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const LoaderSpan = styled.span<any> `
+    width: ${props => props.width};
+    height: ${props => props.height};
+    border: ${props => `${props.thickness} solid ${props.border}`};
+    border-bottom-color: ${props => props.contrast};
+    border-radius: 50%;
+    display: inline-block;
+    box-sizing: border-box;
+    animation: rotation 1s linear infinite;
+
+
+    @keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    } 
+}
+`
+
+type LoaderProps = {
+    size: "sm" | "md" | "lg"
+    variant: "primary" | "secondary"
+}
+
+const sizes = {
+    sm: {
+        width: "12px",
+        height: "12px",
+        thickness: "2px",
+    },
+    md: {
+        width: "18px",
+        height: "18px",
+        thickness: "3px",
+    },
+    lg: {
+        width: "24px",
+        height: "24px",
+        thickness: "4px",
+    }
+}
+
+const variants = {
+    primary: {
+        border: "#cbcbcb",
+        contrast: "#f00"
+    },
+    secondary: {
+        border: "#cbcbcb",
+        contrast: "#f00"
+    }
+}
+
+const Loader: React.FC<LoaderProps> = ({ size, variant }) => {
+    return <LoaderSpan {...sizes[size]} {...variants[variant]} />
+}
+
+export default Loader;
